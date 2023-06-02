5303978557b8f6d93554449357530c60eddeb173
cubeiplkumar <emailanilkumarm@gmail.com>
2023-06-02 12:45:35 +0530

    New Modification on HBS.php file

--- HealthBondScheme.php
+++ HealthBondScheme.php
@@ -7,3 +7,8 @@ bond_purchaseDate
 new modifications by developer4
 added new policies into this bond
 customized requirements to be included...by client
+
+Modifications-1
+
+UserCategory
+Registration_Category
