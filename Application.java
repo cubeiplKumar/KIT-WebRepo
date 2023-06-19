69d1cb2c2e9aa394dee5589f13f0235b3474f076
cubeiplkumar <emailanilkumarm@gmail.com>
2023-06-19 12:48:21 +0530

    Modified Application.java file by dev4

--- Application.java
+++ Application.java
@@ -21,5 +21,26 @@ public class ReadAndPrintScores
     // return value
     return sum;
   }
-		
+#for Github Webhook job demo following changes done in the code
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("newFile.txt");
+
+    try {
+
+      // trying to create a file based on the object
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }		
 }
